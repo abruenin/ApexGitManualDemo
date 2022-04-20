ca701a32eebfc03f0406478f59c5d55e77433402
abruenin <abruenin@gmail.com>
2022-04-20 15:44:32 +0200

    Added connection test

--- bash/add-to-tnsnames.sh
+++ bash/add-to-tnsnames.sh
@@ -1,1 +1,14 @@
+# Adds tnsnames entries
+# set DBUSER & DBPASS variables for connection test
+
 cat add-tnsnames.ora >> $TNS_ADMIN/tnsnames.ora
+
+sql $DBUSER/$DBPASS@dev1 <<EOF
+sho pdbs
+quit
+EOF
+
+sql $DBUSER/$DBPASS@dev2 <<EOF
+sho pdbs
+quit
+EOF
